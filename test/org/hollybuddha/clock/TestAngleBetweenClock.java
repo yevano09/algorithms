734bd2a465002bfdcc620f9daf7f08384c457977
yevano09 <jawahar.m@gmail.com>
2014-04-04 10:27:33 +0530

    adding test cases

---
+++ test/org/hollybuddha/clock/TestAngleBetweenClock.java
@@ -0,0 +1,14 @@
+package org.hollybuddha.clock;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAngleBetweenClock {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
